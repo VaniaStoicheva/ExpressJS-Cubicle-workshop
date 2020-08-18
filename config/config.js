a3cbf8720ad21c463f682fede2f6baa427deccbd
VaniaStoicheva <stoychevavania@gmail.com>
2020-08-18 11:46:42 +0300

    feat: implement login

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@ module.exports = {
     development: {
         port: process.env.PORT || 3000,
         databaseUrl:`mongodb+srv://user:Vanina77@expressjs-20.bdqqj.mongodb.net/cubicle?retryWrites=true&w=majority`
+        //databaseUrl:'mongodb://localhost:27017/',
     },
     production: {}
 };
--- controllers/user.js
+++ controllers/user.js
@@ -1,27 +1,63 @@
 const User=require('../models/user')
 const bcrypt=require('bcrypt')
+const  jwt  = require('jsonwebtoken')
+const privateKey='PRIVATE-KEY11';
 
 const saveUser=async(req,res)=>{
     const{
         username,
         password
     }=req.body
+    
     //hashing
     const salt=await bcrypt.genSalt(10)
-    
-        const hashedPassword=await bcrypt.hash(password,salt)
+    const hashedPassword=await bcrypt.hash(password,salt)
    
-           const user=new User({
+    const user=new User({
                username,
                password:hashedPassword
            })
    
-           await user.save()
-   
-           return true
-        
+    const userObject= await user.save()
+    
+    const token= jwt.sign({
+        userId:userObject._id,
+        username:userObject.username
+    }, privateKey)
+
+    res.cookie('authid',token)
+
+    return true       
+}
+
+const verifyUser= async(req,res)=>{
+
+    const{
+        username,
+        password
+    }=req.body
+
+    const user=await User.findOne({ username })
+
+    const status=await bcrypt.compare(password,user.password)
+
+    console.log(status)
+
+    if(status){
+        const token= jwt.sign({
+            userId:user._id,
+            username:user.username
+        }, privateKey)
+    
+        res.cookie('authid',token)
+    }
+    
+    
+
+    return status
 }
 
 module.exports={
-    saveUser
+    saveUser,
+    verifyUser
 }
